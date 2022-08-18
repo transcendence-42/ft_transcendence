281717ef9bd184dc6fdccaf2c5cde0b48ead6fab
Justine NOUGAROU <jnougaro@e1r1p8.clusters.42paris.fr>
2022-08-18 16:49:32 +0200

    welcome page + redirection to 42 authentification

---
+++ srcs/client/src/components/emailAuth.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+function EmailAuth() {
+    return (
+        <div className='one'>
+            <button className='email btn btn-light'>email authentification</button>
+        </div>
+    );
+}
+
+export default EmailAuth;
