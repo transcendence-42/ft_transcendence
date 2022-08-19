66edc94effa28ac4194961056a541b7333e931d9
Noufel <noufel.ammari@hotmail.com>
2022-08-19 14:44:02 +0200

    fixing bug on  prisma to delete all entreis for testing purposes

--- srcs/server/src/prisma/prisma.service.ts
+++ srcs/server/src/prisma/prisma.service.ts
@@ -15,7 +15,15 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
   async cleanDatabase() {
     if (process.env.NODE_ENV == 'production') return;
-    const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
-    return Promise.all(models.map((modelKey) => this[modelKey].deleteMany()));
-  }
+    // await this.user.deleteMany({});
+    // await this.credentials.deleteMany({});
+    // const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
+    // return Promise.all(models.map((modelKey) => this[modelKey].deleteMany()));
+    const propertyNames = Object.getOwnPropertyNames(this);
+    const modelNames = propertyNames.filter(
+      (propertyName) => !propertyName.startsWith('_')
+    );
+  
+    return Promise.all(modelNames.map((model) => this[model].deleteMany()));
+  };
 }
