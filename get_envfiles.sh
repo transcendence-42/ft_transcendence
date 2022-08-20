d906d7bc8f09f45f3a2e6c43261fe23c5b5143cb
twagner <twagner@student.42.fr>
2022-08-20 15:36:16 +0200

    merge: removing get envfile script as it is now part of the makefile, and Readme as I thing it is a mistake from previous tests

--- get_envfiles.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-git clone git@github.com:transcendence-42/PRIVATE_env_files_container.git
-
-mv PRIVATE_env_files_container/.env* ./srcs/
-
-rm -rf PRIVATE_env_files_container
--- srcs/README.md
+++
@@ -1,2 +0,0 @@
-# PRIVATE_env_files_container
-This repo contains our .env files 
