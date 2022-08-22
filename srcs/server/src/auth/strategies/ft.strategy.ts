c4e3bf1358127f8d89c2ea1d73af70b26881aee1
Nouf <50541029+lefoun@users.noreply.github.com>
2022-08-22 14:49:36 +0200

    deleted unused comment.
    @jnougaro

--- srcs/server/src/auth/strategies/ft.strategy.ts
+++ srcs/server/src/auth/strategies/ft.strategy.ts
@@ -12,13 +12,6 @@ export class FtStrategy extends PassportStrategy(Strategy, '42') {
       clientID: config.get('CLIENT_ID'),
       clientSecret: config.get('CLIENT_SECRET'),
       callbackURL: config.get('CALLBACK_URL'),
-      //   profileFields: {
-      //     id: function (obj) {
-      //       return String(obj.id);
-      //     },
-      //     username: 'login',
-      //     'emails.0.value': 'email',
-      //     'photos.0.value': 'image_url',
     });
   }
   async validate(accessToken: string, refreshToken: string, profile: any) {
