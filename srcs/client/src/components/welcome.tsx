1c81dcc4cce8f1f6ea815737e6085d77fde71016
Justine NOUGAROU <jnougaro@e2r12p4.clusters.42paris.fr>
2022-08-22 17:46:31 +0200

    files cleaned

--- srcs/client/src/components/welcome.tsx
+++ srcs/client/src/components/welcome.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-
-import EmailAuth from './emailAuth';
-import SchoolAuth from './schoolAuth';
+import React, { useEffect, useState } from "react"
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { isNamespaceExport } from "typescript";
 
 
 function Welcome() {
-    return (
+    
+    // const [data,setData]=useState([]);
+
+    //     const [statusMessage, setStatusMessage] = useState("TBD");
+      
+    //     const fetchData = () => {
+    //       fetch("http://localhost:4200/auth/42/redirect/")
+    //         .then((response) => response.json())
+    //         .then((response) => {
+    //           setStatusMessage(response.status);
+    //         })
+    //         .catch(() => {
+    //           setStatusMessage("ERROR");
+    //         });
+    //         console.log()
+    //     };
+  return (
         <div className='main'>
             <nav className='navBar'>
                 <h2>Pong.</h2>
+            <a href="http://localhost:4200/auth/42/redirect/">
                 <button className='button'>Sign in</button>
+            </a>
             </nav>
             <div className='description'>
                 <h1>
