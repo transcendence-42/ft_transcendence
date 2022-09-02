60814eccecd9207b054e7796043c96df4e43a4f5
Noufel AMMARI <nammari@e1r4p4.clusters.42paris.fr>
2022-09-02 14:41:11 +0200

    feat: beginning of player movement

--- srcs/client/src/components/Game.tsx
+++ srcs/client/src/components/Game.tsx
@@ -1,39 +1,71 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
-import { SocketContext } from '../socket';
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { SocketContext } from "../socket";
+import { Stage, Layer, Rect, Circle } from "react-konva";
+
+enum movement {
+  UP = 0,
+  DOWN
+}
 
 const Game = () => {
   const socket = useContext(SocketContext);
-  const [players, setPlayers] = useState([]);
+  const [game, setGame] = useState({} as any);
+
+  const handleMove = (event: any) => {
+    if (event.key === "w" || event.key === "W") {
+      socket.emit("move", { move: movement.UP });
+    }
+    if (event.key === "s" || event.key === "S") {
+      socket.emit("move", { move: movement.DOWN });
+    }
+  };
+  const handleGameUpdate = useCallback((gameData: any) => {
+    setGame(gameData);
+  }, []);
 
   const handleConnect = useCallback(() => {
-    console.log('connection from client');
+    console.log(`connection from client: ${socket.id}`);
   }, []);
 
   const handleBroadcast = useCallback((data: any) => {
-    console.log('Broadcasted : ' + data.message );
-    setPlayers(data.clients);
-  }, []);
-  
-  useEffect(() => {
-    socket.on('broadcast', handleBroadcast);
-    socket.on('connect', handleConnect);
+    console.log("recieved broadcast: " + data.message);
+    setGame(data.game);
   }, []);
 
-  const playersRect = players.map((player: any, index: number) => <Rect key={player.socketId} width={50} height={50} x={100 * (index + 1)} fill="red" />) 
-  
+  useEffect(() => {
+    socket.on("broadcast", handleBroadcast);
+    socket.on("updateGame", handleGameUpdate);
+    socket.on("connect", handleConnect);
+    document.addEventListener("keydown", handleMove);
+  }, []);
+
+  let playersRect = [];
+  if (game && game.players)
+    playersRect = game.players.map((player: any, index: number) => {
+      if (player)
+        return (
+          <Rect
+            key={player.socketId}
+            width={10}
+            height={50}
+            y={player.y}
+            x={player.x}
+            fill="red"
+          />
+        );
+      else return;
+    });
+
   return (
     <div>
       <Stage width={600} height={600}>
         <Layer name="background">
           <Rect width={600} height={600} fill="blue" />
         </Layer>
-        <Layer>
-          {playersRect}
-        </Layer>
+        <Layer>{playersRect}</Layer>
       </Stage>
     </div>
-  )
-}
+  );
+};
 
 export default Game;
--- srcs/server/src/game/game.gateway.ts
+++ srcs/server/src/game/game.gateway.ts
@@ -1,36 +1,134 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   WebSocketGateway,
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+class Game {
+  constructor() {
+    this.players = [];
+    this.ball = new Ball();
+  }
+  players: Player[];
+  ball: Ball;
+}
+
+class Player {
+  constructor(readonly position: PlayerPosition, readonly socketId: string) {
+    if (position === PlayerPosition.LEFT) {
+      this.x = 550;
+    } else this.x = 50;
+    this.y = 275;
+  }
+  x: number;
+  y: number;
+  positon: PlayerPosition;
+}
+
+class Ball {
+  constructor() {
+    this.x = 300;
+    this.y = 300;
+  }
+  x: number;
+  y: number;
+  direction;
+  speed: number;
+}
+
+enum Movement {
+  UP = 0,
+  DOWN,
+}
+
+enum PlayerPosition {
+  LEFT = 0,
+  RIGHT,
+}
+
 @WebSocketGateway(4343, { cors: true })
-export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class GameGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
+{
   private numClient = 0;
-  private clients = [];
+  private game: Game = new Game();
+  private readonly moveSpeed: number = 5;
+  private readonly canvaX = 600;
+  private readonly canvaY = 600;
+  private readonly barHeight = 50;
 
   @WebSocketServer()
   server: Server;
 
+  onModuleInit() {
+    console.log('Websocket server is up...');
+  }
+
   handleConnection(client: Socket, payload: any) {
-    console.log(`client connected`);
-    console.log(`client number : ` + (+this.clients.length + 1));
-    // add client to the client array
-    this.clients.push({ id: this.clients.length + 1, socketId: client.id });
+    console.log(`client number : ${client.id} connected to the server`);
+    // add player to the players array in the Game
+    if (!this.game.players[PlayerPosition.LEFT])
+      this.game.players[PlayerPosition.LEFT] = new Player(
+        PlayerPosition.LEFT,
+        client.id,
+      );
+    else
+      (this.game.players[PlayerPosition.RIGHT] = new Player(
+        PlayerPosition.RIGHT,
+        client.id,
+      )),
+        this.server.emit('broadcast', {
+          message: `${client.id} joined the server`,
+          game: this.game,
+        });
+    if (this.game.players[0] && this.game.players[1])
+      setInterval(() => {
+        this.server.emit('updateGame', this.game);
+      }, 1000 / 60);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`client ${client.id} disconnected`);
+    this.game.players = this.game.players.map((player) => {
+      if (player && player.socketId !== client.id) return null;
+      else return player;
+    });
     this.server.emit('broadcast', {
-      message: `${client.id} joined the server`,
-      clients: this.clients,
+      message: `${client.id} left the server`,
+      game: this.game,
     });
   }
 
-  handleDisconnect(client: any) {
-    console.log(`client disconnected`);
-    this.clients = this.clients.filter((cli) => cli.socketId !== client.id);
-    this.server.emit('broadcast', {
-      message: `${client.id} leaved the server`,
-      clients: this.clients,
+  @SubscribeMessage('move')
+  handleMove(client: Socket, payload: { move: number }) {
+    this.game.players = this.game.players.map((player) => {
+      if (player && player.socketId === client.id) {
+        console.log(`Moving client ${JSON.stringify(player, null, 4)}`);
+        return this._physicsEngine(player, payload.move);
+      } else return player;
     });
+  }
+
+  private _updateGame(server, game) {
+    server.emit('updateGame', this.game);
+  }
+
+  private _ballPhysics(clients: Player[]) {
+    // apply physics.js
+    //
+  }
+
+  private _physicsEngine(client: Player, move: number): Player {
+    const yLimitUp = this.moveSpeed;
+    const yLimitDown = 600 - this.moveSpeed - this.barHeight;
+    if (move === Movement.UP && client.y >= yLimitUp)
+      client.y -= this.moveSpeed;
+    if (move === Movement.DOWN && client.y <= yLimitDown)
+      client.y += this.moveSpeed;
+    return client;
   }
 }
