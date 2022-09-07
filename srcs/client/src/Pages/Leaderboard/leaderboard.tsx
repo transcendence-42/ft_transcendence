b7ff67839e24037e35a09bedf0eaa11b4935e482
Florian Mastorakis <florian.mastorakis@hotmail.fr>
2022-09-07 19:07:48 +0200

    leaderboard is working, not crashing anymore event when user.stats is not init, need to implement index

--- srcs/client/src/Pages/Leaderboard/leaderboard.tsx
+++ srcs/client/src/Pages/Leaderboard/leaderboard.tsx
@@ -3,6 +3,7 @@ import {getFetch} from './getFetch'
 import { useLocation } from "react-router-dom";
 import {useState, useEffect } from 'react'
 import React from 'react';
+import { render } from '@testing-library/react';
 
 export default function Leaderboard () {
   
@@ -20,43 +21,80 @@ export default function Leaderboard () {
         setUsers(responseObject);
       });
   },[userID]);
-  if(users)
+
+
+
+ if(users)
   {
     return (
-      <>
-      <div className="leaderboard" data-testid="tracker">
-          <div className='frame'>
-            <div>
-              <h1 className="pinkText " style={{fontSize: "4vw"}}>LEADERBOARD</h1>
-            </div>
-            <div className='container'>
-              {
-                users && users.map((user : any, index: number) => (
-                <div key={index}>
-                <div className='blueText' style={{fontSize: "2vw"}}>
-                  <h2>RANK</h2>
-                  <div className="item-container"> {user.username}</div>
+      
+          <>
+            <div className="leaderboard" data-testid="tracker">
+              <div>
+              <div className='frame'>
                 <div>
-
-                  </div>
+                  <h1 className="pinkText " style={{fontSize: "4vw"}}>LEADERBOARD</h1>
+                </div>
+                <div className='container'>
+                <div className='blueText' style={{fontSize: "2vw"}}>
+                  <h2 className="column" >RANK</h2>
                 </div>
                 <div className='blueText' style={{fontSize: "2vw"}}>
-                  <h2>NAME</h2>
-                  <div className="item-container"> {user.username} 
-                  </div> 
-                </div>
+                    <h2 className="column">NAME</h2>
+                </div> 
                 <div className='blueText' style={{fontSize: "2vw"}} >
-                  <h2>WINS</h2>
-                  <div className="item-container"> {user.stats && user.stats.wins} 
-                  </div>
+                  <h2 className="column">WINS</h2>
+                </div>
+                </div> 
+                <div className='container'>
+                  <table>
+                  <tbody>
+                    { 
+                     users &&
+                    users.sort((a: {
+                      wins: any; stats: { wins: any; }; },b: {wins: any; stats: { wins: any; };}) => (a.stats && a.stats.wins) < (b.stats && b.stats.wins) ? 1 : -1)
+                    .map((user: { id: React.Key; username: string ; stats: { wins: any; }; })  =>
+                              <tr key={user.id}>
+                                  <td style={{fontSize: "2vw"}}>{user.id}</td>
+                                  <td style={{fontSize: "2vw"}}>{user.username}</td>
+                                  <td style={{fontSize: "2vw"}}>{user.stats && user.stats.wins}</td>
+                              </tr>
+                          )}
+                    </tbody> 
+                  </table>
                 </div>
               </div>
-                ))} 
+              </div>
             </div>
-          </div>
-      </div>
-           </>
+          </>
         );
 }
 return(<></>);
 }
+
+{/* <tbody>
+{ users &&
+users.sort((a: { id: number; },b: { id: number; }) =>a.id > b.id ? a : -1)
+.map((user: { id: React.Key; username: string ; stats: { wins: any; }; })  =>
+          <tr key={user.id}>
+              <td style={{fontSize: "2vw"}}>{user.id}</td>
+              <td style={{fontSize: "2vw"}}>{user.username}</td>
+              <td style={{fontSize: "2vw"}}>{user.stats && user.stats.wins}</td>
+          </tr>
+      )}
+</tbody> */}
+
+// FOR WINS
+{/* <tbody>
+{ 
+ users &&
+users.sort((a: {
+  wins: any; stats: { wins: any; }; },b: {wins: any; stats: { wins: any; };}) => a.wins > b.wins ? 1 : -1)
+.map((user: { id: React.Key; username: string ; stats: { wins: any; }; })  =>
+          <tr key={user.id}>
+              <td style={{fontSize: "2vw"}}>{user.id}</td>
+              <td style={{fontSize: "2vw"}}>{user.username}</td>
+              <td style={{fontSize: "2vw"}}>{user.stats && user.stats.wins}</td>
+          </tr>
+      )}
+</tbody> */}
