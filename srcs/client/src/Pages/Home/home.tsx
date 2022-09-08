1c688088c70eaef53f2f1b5ac6885daa22628f4a
Florian Mastorakis <florian.mastorakis@hotmail.fr>
2022-09-08 19:38:01 +0200

    new welcome page with animation (not connected)

--- srcs/client/src/Pages/Home/home.tsx
+++ srcs/client/src/Pages/Home/home.tsx
@@ -7,8 +7,50 @@ import PongIMG from "./Pong.jpg";
 
 export default function Home () {
 
+  useEffect(() => {
+    const data = window.localStorage.getItem("StillConnected");
+    if (data)
+    {
+        setIsLogged(JSON.parse(data));
+    }
+
+  }, []);
+
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    window.localStorage.setItem("StillConnected", JSON.stringify(isLogged));
+}, []);
+console.log(isLogged);
+
+if (!isLogged)
+{
     return (
         <>
+        <div className="home" data-testid="tracker" >
+          <div className="title">
+            <h2 className="pinkText " style={{fontSize: "4vw"}}> ENTER THE PONG CONTEST ! </h2>  
+          </div>   
+            <div className="homeElement1">
+              <h2 className="blueText" style={{fontSize: "1.5vw"}}>  Confront other players
+                                                                  online and become the best at Pong! </h2>
+            </div>
+            <div className="field">
+              <div className="net"></div>
+              <div className="ping"></div>
+              <div className="pong"></div>
+              <div className="ball"></div>
+            </div>
+            <div className="homeElement2">
+              <h2 className="blueText" style={{fontSize: "2vw"}}> Join Players From 42 School </h2>
+            </div>
+          </div>
+         </>
+    );
+  }
+  else
+  {
+    return (
+      <>
         <div className="home" data-testid="tracker" >
           <div className="homeElement">
             <h2 className="pinkText " style={{fontSize: "4vw"}}> ENTER THE PONG CONTEST ! </h2>
@@ -22,7 +64,7 @@ export default function Home () {
             <h2 className="blueText" style={{fontSize: "2vw"}}> Join Players From 42 School </h2>
             </div>
         </div>
-         </>
-    );
+       </>
+  );
 }
-
+}
