09b218009ecfe3b0350242b337a3f0b84633e97d
Noufel AMMARI <nammari@e1r2p10.clusters.42paris.fr>
2022-09-15 16:43:32 +0200

    renamed branch

--- srcs/client/src/Socket.tsx
+++ srcs/client/src/Socket.tsx
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client';
-import { createContext } from 'react';
 
 export const socket = io(process.env.REACT_APP_CHAT_SOCKET_URL as string, {
   withCredentials: true
 });
-export const socketContext = createContext(socket);
-
-console.log('Thsi is socket url ', process.env.REACT_APP_CHAT_SOCKET_URL);
