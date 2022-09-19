934f0394869511495c247cf4f69c7590b48a2e69
Justine NOUGAROU <jnougaro@e1r11p6.clusters.42paris.fr>
2022-09-19 16:10:18 +0200

    save channel en cours

--- srcs/client/src/Pages/Chat/chat.tsx
+++ srcs/client/src/Pages/Chat/chat.tsx
@@ -8,25 +8,75 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 export default function chat() {
     
     return (
+        
         <div className='container'>
-            <div className='row'>
-                <div className='col-2 rounded-pill'>
-                    <div className='col'>
-                        <p className='titles'>Channels</p>
+            <div className='row row-color'>
+                <div className='col-2 rounded-4 vh-100 blue-box-chat'>
+                    <div className='row'>
+                        <div className='col'>
+                            <br></br>
+                            <p className='yellow-titles'>Channels</p>
+                        </div>
+                        <div className='col'>
+                            <br></br>
+                            <button className='plus float-end data-toggle="modal" data-target="#exampleModal" rounded-pill'>+</button>
+                        </div>
                     </div>
-                    <div className='col'>
-                        <button className='plus float-end data-toggle="modal" data-target="#exampleModal" rounded-pill'>+</button>
+                    <div className='row'>
+                        <div className='col overflow-auto scroll-bar'>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                            <p>Channel name</p>
+                        </div>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className='row'>
+                        <div className='col'>
+                            <p className='yellow-titles'>Messages</p>
+                        </div>
+                        <div className='col'>
+                            <button className='plus float-end data-toggle="modal" data-target="#exampleModal" rounded-pill'>+</button>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col overflow-auto scroll-bar'>
+                            <p>Message</p>
+                            <p>Message</p>
+                            <p>Message</p>
+                            <p>Message</p>
+                            <p>Message</p>
+                            <p>Message</p>
+                            <p>Scroll</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='col-2 rounded-pill'>
-                    <div className='col'>
-                        <p className='titles'>Direct messages</p>
+                <div className='col-8 rounded-4 blue-box-chat'>
+                    <div className='row'>
+                        <div className='col'>
+                            <br></br>
+                            <p className='blue-titles'>Channel Name</p>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col'>
+                            <input type='text' className='rounded-4 input-field-chat yellow-box-chat' placeholder="Send a message..."></input>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-2 rounded-4 blue-box-chat'>
+                    <div className='row'>
+                        <div className='col'>
+                            <br></br>
+                            <p className='blue-titles center-position'>Members</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
