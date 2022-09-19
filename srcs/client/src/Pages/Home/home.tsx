2b28fe13cca6844e48a45fca9e79718788252dc1
Florian MASTORAKIS <flmastor@e1r8p5.clusters.42paris.fr>
2022-09-19 19:19:20 +0200

    comment and Elo Rating added

--- srcs/client/src/Pages/Home/home.tsx
+++ srcs/client/src/Pages/Home/home.tsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Context from "../../Context/Context";
 import './home.css';
 import "../../Components/Tools/Box.css"
 import "../../Components/Tools/Text.css"
-import { Link } from "react-router-dom";
 import "../../Components/Tools/VirtualPong/virtualPong.css"
-import Context from "../../Context/Context";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 export default function Home () {
@@ -82,22 +84,27 @@ if (!(contextValue.isConnected))
 {
     return (
     <>
-       <div className="title1" data-testid="tracker">
-          <h2 className="pinkText " style={{fontSize: "4vw"}}> ENTER THE PONG CONTEST ! </h2>  
-        </div>   
-          <div className="homeElement1">
-            <h2 className="blueText" style={{fontSize: "1.5vw"}}>  Confront other players
-                                                                online and become the best at Pong! </h2>
-          </div>
-          <div className="containerForField">
-            <div className="field">
-              <div className="net"></div>
-              <div className="ping"></div>
-              <div className="pong"></div>
-              <div className="ball"></div>
-
-          </div>
+    
+        <div >
+          <h1 className="h1 pinkText">ENTER THE PONG CONTEST !</h1>
         </div>
+        <div className=" blueText">
+          <br />
+          <h4 >Confront other players online and become the best at Pong! </h4>
+        </div>
+        <div className=" container">
+          <div className="col-size field">
+            <div className="net"></div>
+            <div className="ping"></div>
+            <div className="pong"></div>
+            <div className="ball"></div>
+           </div>
+        </div>
+        <div className=" blueText   ">
+          <br />
+          <h5 className="text-center" >Join Players From 42 School</h5>
+        </div>
+      
     </>
     );
   }
@@ -129,5 +136,26 @@ if (!(contextValue.isConnected))
        </>
   );
 }
-
 }
+
+
+// return (
+//   <>
+//      <div className="title1" data-testid="tracker">
+//         <h2 className="pinkText " style={{fontSize: "4vw"}}> ENTER THE PONG CONTEST ! </h2>  
+//       </div>   
+//         <div className="homeElement1">
+//           <h2 className="blueText" style={{fontSize: "1.5vw"}}>  Confront other players
+//                                                               online and become the best at Pong! </h2>
+//         </div>
+//         <div className="containerForField">
+//           <div className="field">
+//             <div className="net"></div>
+//             <div className="ping"></div>
+//             <div className="pong"></div>
+//             <div className="ball"></div>
+
+//         </div>
+//       </div>
+//   </>
+//   );
