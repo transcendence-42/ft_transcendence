3caefbebe55fc8fd3836b2aa77a0defab910954a
Florian Mastorakis <florian.mastorakis@hotmail.fr>
2022-09-22 14:30:33 +0200

    fixe home page

--- srcs/client/src/App.tsx
+++ srcs/client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home/home'
 import Profile from './Pages/Profile/Profile'
