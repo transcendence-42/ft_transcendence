8b867d03de6b6c4d0aeb5639acff3a215eb72d67
Florian MONBEIG <fmonbeig@e1r6p3.clusters.42paris.fr>
2022-09-29 12:54:33 +0200

    debut double auth

--- srcs/client/src/Pages/Profile/Button/DoubleAuth.tsx
+++ srcs/client/src/Pages/Profile/Button/DoubleAuth.tsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import "../../../Components/Tools/Text.css"
 import "../../../Components/Tools/Box.css"
+import ModalDoubleAuth from "../Modal/ModalDoubleAuth"
+import { useState } from "react";
 
 
-export default function DoubleAuth() {
+export default function DoubleAuth(props : any) {
 
+	const [isShowing, setIsShowing] = useState(false);
+
+	function toggle() {
+		setIsShowing(!isShowing);
+	}
 	return (
-		<div className="yellowPinkBoxButtonProfil"
+		<>
+			<button className="yellowPinkBoxButtonProfil"
 			style={{
 				width: "100%",
 				height: "auto",
-			}}>
-			<div className="blueText" style={{fontSize: "1vw"}}>
-				Double Authentification Factor
-			</div>
-		</div>
+			}}
+			onClick={()=>(toggle())}
+			>
+				<div className="blueText" style={{fontSize: "1vw"}}>
+					Double Authentication
+				</div>
+			</button>
+				<ModalDoubleAuth
+				isShowing={isShowing}
+				hide={toggle}
+				id={props.id}
+				up={props.up}
+				title="Put an URL of your New Picture"
+			 />
+		</>
  		);
 }
+
+
