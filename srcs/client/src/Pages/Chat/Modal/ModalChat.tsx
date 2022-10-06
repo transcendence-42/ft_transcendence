a42c4b7fc06004ca71eee9a149c646f96945457e
Noufel Ammari <noufel.ammari@hotmail.com>
2022-10-06 15:52:24 +0200

    feat: fixed minor bug on friends displaying loading

--- srcs/client/src/Pages/Chat/Modal/ModalChat.tsx
+++ srcs/client/src/Pages/Chat/Modal/ModalChat.tsx
@@ -84,6 +84,7 @@ export default function ModalChat({
           userId={user?.id}
           friends={friends}
           createDirect={createDirect}
+          userChannels={user?.channels}
         />
       </BrowseModal>
       {/* ADD FRIEND */}
